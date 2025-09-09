b1f6b394fd98ebc8e5a71328ac510d66e54c55e3
Bholasankar nanda <bholasankarnanda592@gmail.com>
2025-09-09 17:08:27 +0530

    Project added only styling left

--- Frontend/src/components/Project.jsx
+++ Frontend/src/components/Project.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import project1Img from "../assets/project1.avif";
 const Project = () => {
   return (
-    <section className="w-full h-full px-4 sm:px-8 md:px-12 lg:pb-0 md:pb-0 py-0 border-2 border-black">
+    <section className="w-full h-full px-4 sm:px-8 md:px-12 lg:pb-0 md:pb-0 py-0">
+      {/* Border */}
       <div className="w-full border-b-2 border-[#151515] mt-4"></div>
-      <div className="w-full h-full flex justify-between items-center flex-col-reverse lg:flex-row md:flex-row border-2 border-amber-600">
-        <div className="w-2/5 border-2 border-black h-full flex justify-between flex-col">
-          <div className="">
+      {/* Wrapper div */}
+      <div className="w-full h-full flex justify-between items-center flex-col-reverse lg:flex-row md:flex-row">
+        {/* Left div */}
+        <div className="w-2/5 h-full flex justify flex-col">
+          <div className="flex justify-start w-full h-full">
             <p>Hello project</p>
           </div>
           <div>
@@ -14,6 +17,8 @@ const Project = () => {
             <button>Web Design</button>
           </div>
         </div>
+
+        {/* Right div */}
         <div className="w-3/5 h-1/4">
           <img src={project1Img} alt="" />
         </div>
