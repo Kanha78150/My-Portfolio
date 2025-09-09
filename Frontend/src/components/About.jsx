6f29455fc12784d8f9410d055a0a52a5f96a307c
Bholasankar nanda <bholasankarnanda592@gmail.com>
2025-09-09 17:21:53 +0530

    About section started

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="w-full h-full px-4 sm:px-8 md:px-12 lg:pb-0 md:pb-0 py-0">
+      {/* Border */}
+      <div className="w-full border-b-2 border-[#151515] mt-4"></div>
+      {/* Wrapper div */}
+      <div>
+        {/* Left div */}
+        <div></div>
+
+        {/* Right div */}
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
